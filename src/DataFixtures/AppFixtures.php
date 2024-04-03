6ea9b673648709cfae16f4c2dc1388cb6db76212
RomainNgd <romain.nigond@gmail.com>
2024-04-03 22:39:26 +0200

    fixtures : update Resource

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,7 +4,9 @@
 namespace App\DataFixtures;
 
 use App\Entity\Comment;
+use App\Entity\RelationType;
 use App\Entity\Ressource;
+use App\Entity\RessourceCategory;
 use App\Entity\RessourceType;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -43,25 +45,47 @@ class AppFixtures extends Fixture
         $modo->setPassword($this->userPasswordHasher->hashPassword($userAdmin, "password"));
         $manager->persist($modo);
 
-        $names = ['video', 'photo', 'article', 'jeux'];
+        $names = ['video', 'photo', 'article', 'jeux', 'defi'];
         $listTypes = [];
-        for ($i = 0; $i < 5; $i++) {
+        foreach ($names as $name){
             // Création de l'auteur lui-même.
             $type = new RessourceType();
-            $name = array_rand($names);
             $type->setSlug(strtoupper($name));
             $type->setTitle(strtolower($name));
             $manager->persist($type);
             $listTypes[] = $type;
+        }
+        $names = ['ami', 'famille', 'amour', 'proffesionnel'];
+        $relationsTypes = [];
+        foreach ($names as $name){
+            // Création de l'auteur lui-même.
+            $type = new RelationType();
+            $type->setSlug(strtoupper($name));
+            $type->setTitle(strtolower($name));
+            $manager->persist($type);
+            $relationsTypes[] = $type;
+        }
+        $names = ['Development personnel', 'grandir avec les autres', 'apprehender les changements'];
+        $ressourceCategories = [];
+        foreach ($names as $name){
+            // Création de l'auteur lui-même.
+            $type = new RessourceCategory();
+            $type->setSlug(strtoupper($name));
+            $type->setTitle(strtolower($name));
+            $manager->persist($type);
+            $ressourceCategories[] = $type;
         }
         // Création d'une vingtaine de livres ayant pour titre
         $ressources = [];
         for ($i = 0; $i < 50; $i++) {
             $ressource = new Ressource();
             $ressource->setTitle('Ressource ' . $i);
-            $ressource->setDescription('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum condimentum convallis tempor. Donec sodales, enim et pharetra semper, ex eros laoreet lorem, in venenatis nibh sem et odio. Pellentesque suscipit tristique odio quis iaculis. Etiam tincidunt arcu vel justo pellentesque maximus. Aenean urna erat, blandit sed auctor vel, porttitor quis ante. Nulla non neque dui. Fusce vestibulum tortor metus, eget finibus augue ultrices quis. Sed ultricies lorem ornare, ornare massa in,');
+            $ressource->setContent('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum condimentum convallis tempor. Donec sodales, enim et pharetra semper, ex eros laoreet lorem, in venenatis nibh sem et odio. Pellentesque suscipit tristique odio quis iaculis. Etiam tincidunt arcu vel justo pellentesque maximus. Aenean urna erat, blandit sed auctor vel, porttitor quis ante. Nulla non neque dui. Fusce vestibulum tortor metus, eget finibus augue ultrices quis. Sed ultricies lorem ornare, ornare massa in,');
+            $ressource->setDescription('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum condimentum co');
             $ressource->setRessourceType($listTypes[array_rand($listTypes)]);
             $ressource->setUser($userAdmin);
+            $ressource->setRessourceCategory($ressourceCategories[array_rand($ressourceCategories)]);
+            $ressource->setRelationType($relationsTypes[array_rand($relationsTypes)]);
             $manager->persist($ressource);
             $ressources[] = $ressource;
         }
