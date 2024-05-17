880fa58f71877227ce5a9f2c5e9570ac9bf27697
romainNgd <romain.nigond@gmail.com>
2024-05-17 10:29:38 +0200

    feat : rate limiter ddos attack

--- src/EventSubscriber/KernelSubscriber.php
+++ src/EventSubscriber/KernelSubscriber.php
@@ -5,7 +5,9 @@ namespace App\EventSubscriber;
 use ApiPlatform\Symfony\Security\Exception\AccessDeniedException;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\Exception\TooManyRequestsHttpException;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\RateLimiter\RateLimiterFactory;
 
 class KernelSubscriber implements EventSubscriberInterface
 {
@@ -17,10 +19,16 @@ class KernelSubscriber implements EventSubscriberInterface
         $this->apiKey = $_ENV['API_KEY'];
     }
 
-    public function onKernelRequest(RequestEvent $event): void
+    public function onKernelRequest(RequestEvent $event, RateLimiterFactory $apiLimiter): void
     {
         $request = $event->getRequest();
         $path = $request->getPathInfo();
+
+        $limiter = $apiLimiter->create($request->getClientIp());
+
+        if (false === $limiter->consume(1)->isAccepted()) {
+            throw new TooManyRequestsHttpException();
+        }
 
         // Vérifie si la requête est faite pour la page /API
         if ($path === '/api/docs') {
@@ -30,6 +38,8 @@ class KernelSubscriber implements EventSubscriberInterface
         if ($request->headers->get('X-API-Key') !== $this->apiKey) {
             throw new AccessDeniedException('Invalid API key');
         }
+
+
     }
 
     public static function getSubscribedEvents(): array
