bad4eb67f50ba866fee135315d05f4b127a2f1de
RomainNgd <romain.nigond@gmail.com>
2024-09-12 00:35:53 +0200

    fix : deployer upodate after fix ssh key

--- deploy.php
+++ deploy.php
@@ -7,18 +7,20 @@ set('application', 'ressources');
 set('repository', 'git@github.com:RomainNgd/ressource.git');
 
 host('production')
-->setHostname('82.66.110.4')
-->setPort(2222)
-->set('remote_user', 'romain')
-->set('identity_file', '~/.ssh/id_rsa')
-->set('forward_agent', true)  // Utiliser l'agent SSH
-->set('deploy_path', '/var/www/ressource');
+    ->setHostname('82.66.110.4')
+    ->setPort(2222)
+    ->set('remote_user', 'romain')
+    ->set('identity_file', '~/.ssh/id_rsa') // À ajuster si besoin
+    ->set('forward_agent', true)
+    ->set('deploy_path', '/var/www/ressource');
 
 task('deploy', [
-'deploy:prepare',
-'deploy:vendors',
-'deploy:clear_paths',
-'deploy:publish',
+    'deploy:prepare',      // Prépare la structure du déploiement
+    'deploy:vendors',      // Installe les dépendances Composer
+    'deploy:cache:clear',  // Vide le cache Symfony
+    'database:migrate',    // (Optionnel) Migrer la base de données
+    'deploy:symlink',      // Met à jour le lien symbolique pour la nouvelle release
+    'deploy:unlock',       // Déverrouille le déploiement en cas d'erreur
 ]);
 
 after('deploy:failed', 'deploy:unlock');
